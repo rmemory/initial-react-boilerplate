acba228c5c02dbb3483a7ef84036ffe7e3487e7f
Richard Memory <richardfmemory@gmail.com>
2018-06-14 16:16:11 -0600

    Add code splitting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,34 @@ import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import importedComponent from 'react-imported-component';
 
-import Home from './application/Home.jsx';
-import DynamicPage from './application/DynamicPage.jsx';
-import NoMatch from './NoMatch.jsx';
+import Home from './Home.jsx';
+import Loading from './Loading.jsx';
+
+const AsyncDynamicPage = importedComponent(
+	() => import(/* webpackChunkName:'DynamicPage' */ './DynamicPage.jsx'),
+	{
+		LoadingComponent: Loading,
+	},
+);
+const AsyncNoMatch = importedComponent(
+	() => import(/* webpackChunkName:'NoMatch' */ './NoMatch.jsx'),
+	{
+		LoadingComponent: Loading,
+	},
+);
 
 /* Main entry point for Rect app */
-const App = () => {
-	return (
+const App = () =>
+	(
 		<Router>
 			<div>
 				<Switch>
 					<Route exact path="/" component={Home} />
-					<Route exact path="/dynamic" component={DynamicPage} />
-					<Route component={NoMatch} />
+					<Route exact path="/dynamic" component={AsyncDynamicPage} />
+					<Route component={AsyncNoMatch} />
 				</Switch>
 			</div>
 		</Router>
 	);
-};
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Layout from '../Layout.jsx';
+import Layout from './Layout.jsx';
 
 const Home = () =>
 	(
